2d0c09a54938a7e3a4947abadbbe2feacfbc8f3c
vikaswilson1980-creator <vikaswilson1980@gmail.com>
2025-09-27 12:49:52 +0530

    Add files via upload

---
+++ Square roots.py
@@ -0,0 +1,5 @@
+Root=int(input("Enter an number for getting a square root:"))
+#calculate the square of different numbers
+sqrt=Root**0.5
+note1=int(sqrt)
+print("The Square root of the number is:",note1)
